dfec32eadd5c105cb847ee2def06d6c5f0f20923
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2021-02-25 00:30:05 +0000

    Small SOS example in Julia.

---
+++ julia/sos.jl
@@ -0,0 +1,34 @@
+# Following https://sums-of-squares.github.io/sos/
+
+using Pkg
+
+Pkg.add("SumOfSquares")
+Pkg.add("DynamicPolynomials")
+Pkg.add("MosekTools")
+
+using SumOfSquares, DynamicPolynomials, MosekTools
+
+#= EXAMPLE 1 =#
+
+# Using Mosek as the SDP solver
+model = SOSModel(Mosek.Optimizer)
+
+# Create symbolic variables
+@polyvar x y
+p = 2*x^4 + 2*x^3*y - x^2*y^2 + 5*y^4
+
+# We want constraint `p` to be a sum of squares
+con_ref = @constraint model p >= 0
+
+optimize!(model)
+
+# Solution status is `OPTIMAL` which means `p` is a sum of squares
+@show termination_status(model)
+
+# Solution `FEASIBLE_POINT`
+@show primal_status(model)
+
+# Show the decomposition 
+# https://github.com/jump-dev/SumOfSquares.jl/blob/master/examples/Getting%20started.jl
+q = gram_matrix(con_ref)
+@show SOSDecomposition(q)
