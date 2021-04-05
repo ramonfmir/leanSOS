184e74e62bc9ad2fd114ee4394f076c3aed638c6
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2021-04-05 15:00:27 +0100

    Better way of parsing nats.

--- lean/scripts/client.py
+++ lean/scripts/client.py
@@ -1,7 +1,20 @@
 import sys
+from parse import *
+
+def parse_arith(expr):
+    return expr
+
+def parse_expr(expr):
+    try:
+        lhs, rhs = parse("{}<={}", expr)
+        lhs = parse_arith(lhs)
+        rhs = parse_arith(rhs)
+        return lhs + "<=" + rhs
+    except Exception as e:
+        print("Invalid")
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1):
-        print(sys.argv[1])
+        print(parse_expr(sys.argv[1]))
     else:
         print("Error")
--- lean/src/sos.lean
+++ lean/src/sos.lean
@@ -1,7 +1,7 @@
 import system.io
 import data.mv_polynomial.basic
 
-open tactic
+open tactic 
 
 meta def execute (input : string) : tactic string :=
 let path := "/Users/ramon/Documents/experiments/leanSOS/lean/scripts/client.py" in
@@ -12,22 +12,19 @@ unsafe_run_io $ io.cmd {
 --set_option trace.eqn_compiler.elim_match true
 set_option eqn_compiler.max_steps 10000
 
-meta def parse_num : expr → string 
-| `(has_zero.zero) := "0"
-| `(has_one.one) := "1"
-| `(bit0 %%e) := "(2*" ++ (parse_num e) ++ ")"
-| `(bit1 %%e) := "(2*" ++ (parse_num e) ++ "+1)"
-| e := ""
+meta def parse_num : option nat → string 
+| (some n) := to_string n
+| _ := ""
 
 meta def parse_sos : expr → string
 | `(@has_le.le %%α %%inst %%e₁ %%e₂) := (parse_sos e₁) ++ "<=" ++ (parse_sos e₂)
-| `(%%e₁ + %%e₂) :=  (parse_sos e₁) ++ "+" ++ (parse_sos e₂)
-| `(@has_sub.sub %%α %%inst %%e₁ %%e₂) := (parse_sos e₁) ++ "-" ++ (parse_sos e₂)
-| `(- %%e) := "-" ++ (parse_sos e)
-| `(%%e₁ * %%e₂) := (parse_sos e₁) ++ "*" ++ (parse_sos e₂)
-| `(@has_pow.pow _ _ %%P %%e₁ %%e₂) := (parse_sos e₁) ++ "^" ++ (parse_sos e₂)
-| `(mv_polynomial.C %%e) := (parse_num e)
-| `(mv_polynomial.X %%e) := "x(" ++ (parse_num e) ++ ")"
+| `(%%e₁ + %%e₂) :=  "(" ++ (parse_sos e₁) ++ "+" ++ (parse_sos e₂) ++ ")"
+--| `(@has_sub.sub %%α %%inst %%e₁ %%e₂) := (parse_sos e₁) ++ "-" ++ (parse_sos e₂)
+--| `(- %%e) := "-" ++ (parse_sos e)
+| `(%%e₁ * %%e₂) := "(" ++ (parse_sos e₁) ++ "*" ++ (parse_sos e₂) ++ ")"
+--| `(@has_pow.pow _ _ %%P %%e₁ %%e₂) := (parse_sos e₁) ++ "^" ++ (parse_sos e₂)
+| `(mv_polynomial.C %%e) := (parse_num (expr.to_nat e))
+| `(mv_polynomial.X %%e) := "x[" ++ (parse_num (expr.to_nat e)) ++ "]"
 | e := ""
 
 meta def sos_aux (input : expr) : tactic unit := do 
