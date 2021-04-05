ccef62f35ca9b5bd3281b70ae6577db0ae8e2228
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2021-04-05 10:41:13 +0100

    Very basic number parser.

--- lean/src/sos.lean
+++ lean/src/sos.lean
@@ -9,7 +9,15 @@ unsafe_run_io $ io.cmd {
   cmd := "python3", 
   args := [path, input] }
 
-set_option trace.eqn_compiler.elim_match true
+--set_option trace.eqn_compiler.elim_match true
+set_option eqn_compiler.max_steps 10000
+
+meta def parse_num : expr → string 
+| `(has_zero.zero) := "0"
+| `(has_one.one) := "1"
+| `(bit0 %%e) := "(2*" ++ (parse_num e) ++ ")"
+| `(bit1 %%e) := "(2*" ++ (parse_num e) ++ "+1)"
+| e := ""
 
 meta def parse_sos : expr → string
 | `(@has_le.le %%α %%inst %%e₁ %%e₂) := (parse_sos e₁) ++ "<=" ++ (parse_sos e₂)
@@ -18,8 +26,9 @@ meta def parse_sos : expr → string
 | `(- %%e) := "-" ++ (parse_sos e)
 | `(%%e₁ * %%e₂) := (parse_sos e₁) ++ "*" ++ (parse_sos e₂)
 | `(@has_pow.pow _ _ %%P %%e₁ %%e₂) := (parse_sos e₁) ++ "^" ++ (parse_sos e₂)
---| `(mv_polynomial.C %%e) := (parse_sos e)
-| e := to_string e
+| `(mv_polynomial.C %%e) := (parse_num e)
+| `(mv_polynomial.X %%e) := "x(" ++ (parse_num e) ++ ")"
+| e := ""
 
 meta def sos_aux (input : expr) : tactic unit := do 
   m ← execute (parse_sos input),
--- lean/src/test.lean
+++ lean/src/test.lean
@@ -7,6 +7,6 @@ open mv_polynomial
 instance {σ R} [comm_semiring R] [has_le R] : has_le (mv_polynomial σ R) :=
 { le := λ p q, ∀ v, eval v p ≤ eval v q }
 
-example : (C (0 : ℝ)) ≤ (X 1) * (X 1):= begin 
+example : (C (0 : ℝ)) ≤ (X 5) * (X 1):= begin 
   sos, sorry,
 end 
