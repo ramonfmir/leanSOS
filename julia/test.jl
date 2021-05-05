573b2c2d791b514e0102fd7d827218b0c7f09fb5
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2021-05-05 13:44:30 +0100

    Some notes on the Julia experiments from a few days ago.

---
+++ julia/test.jl
@@ -0,0 +1,14 @@
+#SumOfSquares
+#MosekTools
+#DynamicPolynomials 
+#LinearAlgebra ?
+#SparseArrays ?
+
+#R = [rand(-10:10)/rand(1:10) for i=1:10, j=1:10]
+#X = transpose(R) * R
+#@show factorize(X)
+
+#S = SymTridiagonal([3., 4., 5.], [1., 2.])
+#@show ldlt(S)
+
+#@show ldlt(sparse(X))
