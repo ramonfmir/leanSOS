702225c33d5b7cb5a0a3aec6bf35d5ca7ea4fb27
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2021-05-07 13:49:49 +0100

    Tests cleanup and moved stuff to 'to_mathlib'.

--- lean/src/test/float.lean
+++ lean/src/test/float.lean
@@ -4,47 +4,50 @@ import float.div
 
 set_option profiler true
 
--- Test 1: 0 + 1
+-- Test add 1: 0 + 1
 #eval rat.mk 0 1 + rat.mk 1 1 
 #eval float.mk 0 0 + float.mk 1 0 -- OK
 
--- Test 2: 1/1024 + 3/4096
+-- Test add 2: 1/1024 + 3/4096
 #eval rat.mk 1 1024 + rat.mk 3 4096 
 #eval float.mk 1 (-10) + float.mk 3 (-12)
 
--- Test 3: 12345/1048576 + -6789/32768
+-- Test add 3: 12345/1048576 + -6789/32768
 #eval rat.mk 12345 1048576 + rat.mk (-6789) 32768 
 #eval float.mk 12345 (-20) + float.mk (-6789) (-15) 
 
--- Test 4: 0 + 1
+-- Test add 4: 12345789101112/2^100 + -123457891011/2^7898
+#eval rat.mk 12345789101112 (2 ^ 100) + rat.mk (-123457891011) (2 ^ 7898) 
+#eval float.mk 12345789101112 (-100) + float.mk (-1234578910111) (-7898) 
+
+-- Test mul 1: 0 * 1
 #eval rat.mk 0 1 * rat.mk 1 1 
 #eval float.mk 0 0 * float.mk 1 0 
 
--- Test 5: 1/1024 * 3/4096
+-- Test mul 2: 1/1024 * 3/4096
 #eval rat.mk 1 1024 * rat.mk 3 4096 
 #eval float.mk 1 (-10) * float.mk 3 (-12)
 
--- Test 6: 12345/1048576 * -6789/32768
+-- Test mul 3: 12345/1048576 * -6789/32768
 #eval rat.mk 12345 1048576 * rat.mk (-6789) 32768 
 #eval float.mk 12345 (-20) * float.mk (-6789) (-15) 
 
--- Test 4: 0 + 1
+-- Test mul 4: 12345789101112/2^100 * -123457891011/2^7898
+#eval rat.mk 12345789101112 (2 ^ 100) * rat.mk (-123457891011) (2 ^ 7898) 
+#eval float.mk 12345789101112 (-100) * float.mk (-1234578910111) (-7898) 
+
+-- Test div 1: 0 / 1
 #eval rat.mk 0 1 / rat.mk 1 1 
 #eval float.mk 0 0 / float.mk 1 0 
 
--- Test 5: 1/1024 * 3/4096
+-- Test div 2: 1/1024 / 3/4096
 #eval rat.mk 1 1024 / rat.mk 3 4096 
 #eval float.mk 1 (-10) / float.mk 3 (-12)
 
--- Test 6: 12345/1048576 * -6789/32768
+-- Test div 3: 12345/1048576 / -6789/32768
 #eval rat.mk 12345 1048576 / rat.mk (-6789) 32768 
 #eval float.mk 12345 (-20) / float.mk (-6789) (-15) 
 
+-- Test div 4: 12345789101112/2^100 / -123457891011/2^7898
 #eval rat.mk 12345789101112 (2 ^ 100) / rat.mk (-123457891011) (2 ^ 7898) 
-#eval round_up 10 (rat.mk 12345789101112 (2 ^ 100) / rat.mk (-1234578910111) (2 ^ 7898))
-#eval round_up 10 (12345789101112 * (-1234578910111) * 2 ^ (100 - 7898))
-#eval float.mk 12345789101112 (-20) / float.mk (-12345789101112) (-15) 
-#eval rat.mk 12345789101112 (2 ^ 100) + rat.mk (-12345789101112) (2 ^ 7898) 
-#eval float.mk 12345789101112 (-100) + float.mk (-12345789101112) (-7898) 
-
--- Still not quick enough, rounding directly float_raw?
+#eval float.mk 12345789101112 (-100) / float.mk (-1234578910111) (-7898) 
---
+++ lean/src/to_mathlib/matrix.lean
@@ -0,0 +1,94 @@
+import data.matrix.basic
+
+variables {γ : Type*} [fintype γ] [decidable_eq γ]
+variables {μ : Type*} [fintype μ] [decidable_eq μ]
+variables {R : Type*} 
+
+open_locale big_operators
+open_locale matrix
+
+namespace matrix 
+
+-- Properties of symmetric matrices. 
+
+def symmetric (M : matrix γ γ R) : Prop :=
+∀ i j, M i j = M j i
+
+lemma symmetric_sum 
+  [ring R]
+  {A B : matrix γ γ R}
+  (hA : symmetric A) (hB : symmetric B)
+: symmetric (A + B) :=
+begin
+  intros i j, simp, rw [hA i j, hB i j],
+end 
+
+-- Properties of the dot product.
+-- TODO: to_mathlib
+
+variable [linear_ordered_comm_ring R]
+
+lemma dot_product_of_nonneg {v w : γ → R} (hv : 0 ≤ v) (hw : 0 ≤ w) : 0 ≤ dot_product v w :=
+begin 
+  simp only [dot_product], apply finset.sum_nonneg, intros x hx, 
+  exact mul_nonneg (hv x) (hw x),
+end 
+
+lemma vec_mul_self_nonneg (v : γ → R) : 0 ≤ v * v :=
+λ i, mul_self_nonneg (v i)
+
+lemma dot_product_diagonal_mul_vec_eq (d v w : γ → R) :
+dot_product ((diagonal d).mul_vec v) w = dot_product d (v * w) :=
+begin 
+  simp only [dot_product], congr, ext i, 
+  rw [mul_vec_diagonal, pi.mul_apply, mul_assoc],
+end 
+
+lemma dot_product_self_nonneg (v : γ → R) : 0 ≤ dot_product v v := 
+begin
+  simp only [dot_product], apply finset.sum_nonneg, intros x hx, 
+  exact mul_self_nonneg (v x),
+end
+
+lemma dot_product_self_eq_zero_iff (v : γ → R) : dot_product v v = 0 ↔ v = 0 :=
+begin 
+  have hsumz := finset.sum_eq_zero_iff_of_nonneg (λ x hx, mul_self_nonneg (v x)), split,
+  { intros hdp, ext x,
+    have hvx := (hsumz.1 hdp) x (finset.mem_univ x),
+    exact (mul_self_eq_zero.1 hvx), },
+  { intros hvz, erw hsumz, intros x hx, dsimp,
+    erw [congr_fun hvz x], simp, }
+end  
+
+lemma dot_product_self_pos_of_nonzero (v : γ → R) (hvnz : v ≠ 0) : dot_product v v > 0 :=
+begin
+  have h1 := dot_product_self_nonneg v,
+  have h2 := ne_comm.1 ((not_iff_not_of_iff (dot_product_self_eq_zero_iff v)).2 hvnz),
+  exact (lt_of_le_of_ne h1 h2),
+end 
+
+lemma dot_product_transpose (v : γ → R) (K L : matrix μ γ R)
+: dot_product v ((Kᵀ ⬝ L).mul_vec v) = dot_product (K.mul_vec v) (L.mul_vec v) :=
+begin
+  rw ←mul_vec_mul_vec, dsimp [mul_vec, dot_product], 
+  have : ∑ i, (v i) * (∑ j, K j i * ∑ k, L j k * v k) = 
+         ∑ i, (∑ j, (v i) * K j i * ∑ k, L j k * v k),
+  { congr, ext i, rw [finset.mul_sum], congr, ext j, ring, },
+  rw this,
+  have : ∑ i j, v i * K j i * ∑ k, L j k * v k =
+         ∑ i j k, v i * K j i * L j k * v k,
+  { congr, ext i, congr, ext j, rw [finset.mul_sum], congr, ext k, ring, },
+  rw this,
+  have : ∑ i, (∑ j, K i j * v j) * ∑ k, L i k * v k = 
+         ∑ i, (∑ j, K i j * v j * ∑ k, L i k * v k),
+  { congr, ext i, rw [finset.sum_mul], },
+  rw this,
+  have : ∑ i j, K i j * v j * ∑ k, L i k * v k = 
+         ∑ i j k, K i j * v j * L i k * v k,
+  { congr, ext i, congr, ext j, rw [finset.mul_sum], congr, ext k, ring, },
+  rw this,
+  rw [finset.sum_comm],
+  congr, ext i, congr, ext j, congr, ext k, ring,
+end
+
+end matrix 
