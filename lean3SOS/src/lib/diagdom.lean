14b50713dc887f6d408b7b2bce1f8af5bb619958
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2021-05-26 20:23:02 +0100

    Some small improvements in diagdom tactic. Times out with small examples...

--- lean3SOS/src/lib/diagdom.lean
+++ lean3SOS/src/lib/diagdom.lean
@@ -13,7 +13,7 @@ import tactic.linarith
 open_locale big_operators
 
 def diagdom {n : ℕ} (A : matrix (fin n) (fin n) ℚ) : Prop :=
-∀ i, (∑ j, if i ≠ j then A i j else 0) ≤ A i i
+∀ i, (∑ j, if i ≠ j then abs (A i j) else 0) ≤ abs (A i i)
 
 namespace diagdom 
 
@@ -27,7 +27,7 @@ do
   dunfold_target [name.from_string M.to_string] {}
 
 meta def prove_diagdom : tactic unit :=
-`[unfold_matrix, intros i, fin_cases i; simp [fin.sum_univ_succ]; linarith]
+`[unfold_matrix, intros i, fin_cases i; simp only [fin.sum_univ_succ]; dec_trivial]
 
 def I2 : matrix (fin 2) (fin 2) ℚ := 
 ![![1, 0], 
@@ -39,10 +39,32 @@ def I4 : matrix (fin 4) (fin 4) ℚ :=
   ![0, 0, 1, 0], 
   ![0, 0, 0, 1]]
 
+def A : matrix (fin 3) (fin 3) ℚ :=
+![![ 3, -2,  1],
+  ![ 1, -3,  2],
+  ![-1,  2,  4]]
+
+def F : matrix (fin 9) (fin 9) ℚ :=
+![![1, 0.04, 0.05, 0.04, 0.02, 0.01, 0.02, 0.05, 0.03], 
+  ![0.02, 1, 0.02, 0.02, 0.03, 0.02, 0.04, 0.04, 0.02], 
+  ![0.04, 0.02, 1, 0.05, 0.05, 0.04, 0.03, 0.05, 0.03], 
+  ![0.05, 0.04, 0.03, 1, 0.02, 0.05, 0.03, 0.02, 0.03], 
+  ![0.03, 0.03, 0.05, 0.03, 1, 0.02, 0.04, 0.01, 0.03], 
+  ![0.05, 0.04, 0.02, 0.01, 0.03, 1, 0.04, 0.05, 0.04], 
+  ![0.03, 0.04, 0.04, 0.02, 0.02, 0.03, 1, 0.01, 0.01], 
+  ![0.04, 0.03, 0.01, 0.02, 0.05, 0.02, 0.02, 1, 0.02], 
+  ![0.05, 0.01, 0.03, 0.04, 0.04, 0.05, 0.03, 0.03, 1]]
+
 set_option profiler true
+
+set_option timeout 10000000
 
 example : diagdom I2 := by prove_diagdom 
 
-example : diagdom I4 := by prove_diagdom 
+example : diagdom I4 := by prove_diagdom
+
+example : diagdom A := by prove_diagdom
+
+--example : diagdom F := by prove_diagdom
 
 end diagdom
